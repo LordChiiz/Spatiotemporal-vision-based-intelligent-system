bd9fe357ba8aac283c1d80f862c425c21b5ecf42
LordChiiz <ahizechisom7@gmail.com>
2026-02-26 14:45:13 +0100

    Added individual vehicle counting
    Now we can count vehicles like cars, trucks and buses individually and display them

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ model = YOLO("yolov8n.pt")
 video_path = "traffic.mp4"
 cap = cv2.VideoCapture(video_path)
 
+car_count = 0
+truck_count = 0                 #to count cars, trucks, buses individually
+bus_count = 0
+
 counted_ids = set() #container for the counted vehicle ids
 vehicle_count = 0 #increases as cars pass he line
 prev_positions = {} #to track previous positions of vehicles
@@ -45,27 +49,34 @@ while cap.isOpened():
         if prev_y < line_y and center_y > line_y and track_id not in counted_ids:
             counted_ids.add(track_id)
             vehicle_count += 1
+
+            if cls == 2:
+                car_count += 1
+            elif cls == 7:
+                truck_count += 1
+            elif cls == 5:
+                bus_count += 1
+
         prev_positions[track_id] = center_y
 
-    cv2.line(frame, (0, line_y), (width, line_y), (0, 255, 255), 2)  #draw hori line
-
-    cv2.putText(frame, f"Vehicle Count: {vehicle_count}",       
-            (50, 50),           
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1,
-            (0, 255, 0),
-            2)
 
 
     annotated_frame = results[0].plot()
     cv2.line(annotated_frame, (0, line_y), (width, line_y), (0, 255, 255), 2)
 
-    cv2.putText(annotated_frame, f"Vehicle Count: {vehicle_count}",
-            (50, 50),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1,
-            (0, 255, 0),
-            2)
+    cv2.putText(annotated_frame, f"Total Vehicle count: {vehicle_count}", (50, 50),
+        cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+
+    cv2.putText(annotated_frame, f"Cars: {car_count}", (50, 90),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,0), 2)               #display individual counts of cars, buses, trucks on the frame
+
+    cv2.putText(annotated_frame, f"Buses: {bus_count}", (50, 120),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,0), 2)
+
+    cv2.putText(annotated_frame, f"Trucks: {truck_count}", (50, 150),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,0), 2)
+
+
 
     cv2.imshow("Traffic Tracking", annotated_frame)
 
